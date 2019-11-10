1bec06e27b28c591180598ad3589431d26515562
johan <johan.leon@unillanos.edu.co>
2019-11-09 22:11:37 -0500

    MemoSpace 0.0.1

--- MemoSpace/src/pages/Home.tsx
+++ MemoSpace/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle } from '@ionic/react';
 import React from 'react';
 import '../pages/home.css';
 
@@ -6,20 +6,78 @@ import '../pages/home.css';
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
-        </IonToolbar>
+      <IonHeader >
+        <IonTitle className="title">MemoSpace</IonTitle>
       </IonHeader>
       <IonContent className="bg">
-        The world is your oyster.
+        
         <p>
+          No es mucho, pero es trabajo honesto
+          <br></br>
+          <br></br>
           If you get lost, the{' '}
           <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
             docs
           </a>{' '}
           will be your guide
         </p>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+
       </IonContent>
     </IonPage>
   );
