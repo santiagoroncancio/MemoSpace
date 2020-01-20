4428489ede193f0dbba4bf5a2e723c71cfdb4ec3
johan <johan.leon@unillanos.edu.co>
2020-01-20 11:54:04 -0500

    3 niveles

--- MemoSpace3/www/js/medium.js
+++ MemoSpace3/www/js/medium.js
@@ -14,14 +14,9 @@ if(navigator.onLine) {
     'https://cdn.eso.org/images/thumb700x/eso0611a.jpg','https://cdn.eso.org/images/thumb700x/eso0611a.jpg',
     'https://cdn.eso.org/images/thumb700x/eso1919a.jpg','https://cdn.eso.org/images/thumb700x/eso1919a.jpg',
     'https://cdn.eso.org/images/thumb700x/eso1434a.jpg','https://cdn.eso.org/images/thumb700x/eso1434a.jpg',
-    'https://cdn.eso.org/images/thumb700x/eso1245a.jpg','https://cdn.eso.org/images/thumb700x/eso1245a.jpg',
-    'https://cdn.eso.org/images/thumb700x/eso0908a.jpg','https://cdn.eso.org/images/thumb700x/eso0908a.jpg',
-    'https://cdn.eso.org/images/thumb700x/eso1229a.jpg','https://cdn.eso.org/images/thumb700x/eso1229a.jpg',
-    'https://cdn.eso.org/images/thumb700x/eso1907g.jpg','https://cdn.eso.org/images/thumb700x/eso1907g.jpg',
-    'https://cdn.eso.org/images/thumb700x/eso1028a.jpg','https://cdn.eso.org/images/thumb700x/eso1028a.jpg',
-    'https://cdn.eso.org/images/thumb700x/eso1907c.jpg','https://cdn.eso.org/images/thumb700x/eso1907c.jpg'];
+    'https://cdn.eso.org/images/thumb700x/eso1245a.jpg','https://cdn.eso.org/images/thumb700x/eso1245a.jpg'];
 } else {
-    var array_memoria=['1','1','2','2','3','3','4','4','5','5','6','6','7','7','8','8','9','9','10','10','11','11','12','12','13','13','14','14','15','15','16','16','17','17','18','18','19','19','20','20',];
+    var array_memoria=['1','1','2','2','3','3','4','4','5','5','6','6','7','7','8','8','9','9','10','10','11','11','12','12','13','13','14','14','15','15'];
 }
 
     var valor_memoria=[];
